10f0122b48b73119df817d1d86300ecfb1129f57
kugimiya <bp.001@yandex.ru>
2022-04-26 14:57:04 +0400

    Add count field to pagination store

--- src/store/PaginationStore.ts
+++ src/store/PaginationStore.ts
@@ -1,19 +1,26 @@
 import { computed, makeObservable } from 'mobx';
+
 import Store from './Store';
 
 export type PaginationState = {
   limit: number;
   page: number;
+  count: number;
 };
 
 export default class PaginationStore extends Store<PaginationState> {
   constructor() {
-    super({ limit: 10, page: 1 });
+    super({ limit: 10, page: 1, count: 0 });
 
     makeObservable(this, {
       page: computed,
       limit: computed,
+      count: computed,
     });
+  }
+
+  get count(): PaginationState['count'] {
+    return this.state.count;
   }
 
   get page(): PaginationState['page'] {
